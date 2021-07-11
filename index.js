8ba48d2102893f15788b8362b1db9e3bbe16351d
David Matthams <dmatthams@gmail.com>
2021-07-11 13:28:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const github = require('@actions/github');
 const fetch = require('fetch').fetchUrl;
 
 var apiUrl = 'https://api.cloudways.com/api/v1';
+const email = core.getInput('email');
+const api_key = core.getInput('api_key');
 
 try {
     fetch(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`, function(error, meta, body){
