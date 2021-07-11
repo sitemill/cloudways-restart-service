e405a4fda4d79d8b95d30988b4dd8b747b37d06e
David Matthams <dmatthams@gmail.com>
2021-07-11 13:25:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fetch = require('fetch').fetchUrl;
 var apiUrl = 'https://api.cloudways.com/api/v1';
 
 try {
-    fetch(apiUrl + `/oauth/access_token?email=${INPUT_EMAIL}&api_key=${INPUT_API_KEY}`, function(error, meta, body){
+    fetch(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`, function(error, meta, body){
         console.log(body.toString());
     });
 } catch (error) {
