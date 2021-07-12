f61883f3cee5fb75d7b03dede0d6dfda036b8cd7
David Matthams <dmatthams@gmail.com>
2021-07-12 17:22:18 +0100

    serverID

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios').default;
 var apiUrl = 'https://api.cloudways.com/api/v1';
 const email = core.getInput('email');
 const api_key = core.getInput('api_key');
+const server_id = core.getInput('server_id');
 
 
 axios.get(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`)
