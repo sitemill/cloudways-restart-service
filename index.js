4541b6489df0b17a621d09d8eb6798cdfe3daadf
David Matthams <dmatthams@gmail.com>
2021-07-12 17:23:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var apiUrl = 'https://api.cloudways.com/api/v1';
 const email = core.getInput('email');
 const api_key = core.getInput('api_key');
 const server_id = core.getInput('server_id');
-
+const service = core.getInput('service');
 
 axios.get(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`)
     .then(function(response) {
