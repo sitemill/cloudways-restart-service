d3d80f070d2935f64534df09ed2b829486a4c366
David Matthams <dmatthams@gmail.com>
2021-07-12 17:07:23 +0100

    Axios

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const fetch = require('fetch').fetchUrl;
+const axios = require('axios').default;
 
 var apiUrl = 'https://api.cloudways.com/api/v1';
 const email = core.getInput('email');
 const api_key = core.getInput('api_key');
 
 try {
-    fetch(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            console.log(data)
+    axios.get(apiUrl + `/oauth/access_token?email=${email}&api_key=${api_key}`)
+        .then(function (response) {
+            // handle success
+            console.log(response);
         })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 } catch (error) {
     core.setFailed(error.message);
 }
